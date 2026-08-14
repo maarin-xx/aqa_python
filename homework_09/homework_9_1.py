ae30eb72664da4311d81d22df889deb2ad749d60
maarin_xx <fm2801m@gmail.co>
2026-08-14 18:19:41 +0300

    changed homework 9_1

--- homework_09/homework_9_1.py
+++ homework_09/homework_9_1.py
@@ -11,16 +11,16 @@
 """
 class Rhombus:
 
-    def __init__(self, a, angle_a):
+    def __init__(self, side_a, angle_a):
 
-        self.a = a
+        self.side_a = side_a
         self.angle_a = angle_a
         self.angle_b = 180 - angle_a
 
 
     def __setattr__(self, name, value):
 
-        if name == 'a':
+        if name == 'side_a':
 
             if not isinstance(value, (float, int)):
                 raise TypeError('The side of a rhombus can be expressed only by numbers')
@@ -37,25 +37,23 @@ class Rhombus:
             if value <= 0 or value >= 180:
                 raise ValueError('The angle of a rhombus must be between 0 and 180')
 
-            angle_n = 'angle_b' if name == 'angle_a' else 'angle_a' #можемо не знати який кут задано, а який змінюємо
-            if angle_n in self.__dict__:
-                if value + self.__dict__[angle_n] != 180:
-                    raise ValueError('The sum of two adjacent angles of a rhombus must be 180')
+            # angle_n = 'angle_b' if name == 'angle_a' else 'angle_a' #можемо не знати який кут задано, а який змінюємо
+            # if angle_n in self.__dict__:
+            #     if value + self.__dict__[angle_n] != 180:
+            #         raise ValueError('The sum of two adjacent angles of a rhombus must be 180')
+
+            self.__dict__[name] = value
+
+            if name == 'angle_a':
+                self.__dict__['angle_b'] = 180 - value
+            else:
+                self.__dict__['angle_a']= 180 - value
+            return
 
 
         self.__dict__[name] = value
 
     def __str__(self):
-        return f'Rhombus with: a = {self.a}, angle_a = {self.angle_a}, angle_b = {self.angle_b}'
+        return f'Rhombus with: side_a = {self.side_a}, angle_a = {self.angle_a}, angle_b = {self.angle_b}'
 
 
-# r1 = Rhombus(1, 150)
-# r2 = Rhombus('1', 150 )
-# r3 = Rhombus(-1, 150 )
-# r4 = Rhombus(1, 0 )
-# # r5 = Rhombus(1, 180 )
-# # r6 = Rhombus(6, 'f')
-# r7 = Rhombus(1, 170 )
-# print(r7)
-# r7.angle_b=170
-# print(r7)
