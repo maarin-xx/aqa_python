b8858181608344fb8fbe23075037a1bca029d40d
maarin_xx <fm2801m@gmail.co>
2026-09-03 07:18:43 +0300

    edited homework_15_1

--- homework_15/homework_15_1.py
+++ homework_15/homework_15_1.py
@@ -10,38 +10,45 @@ file_path_r_m_c = current_dir / 'r-m-c.csv'
 file_path_rmc = current_dir / 'rmc.csv'
 
 
-def read_csv(file_path):
+def read_csv(file_path, delimiter):
     with open(file_path, newline='') as csvfile:
-        data = list(csv.reader(csvfile, delimiter=','))
+        data = list(csv.reader(csvfile, delimiter=delimiter))
 
     headers = data[0]
     return [dict(zip(headers, row)) for row in data[1:]]
 
-file_1 = read_csv(file_path_r_m_c)
-file_2 = read_csv(file_path_rmc)
+
+file_1 = read_csv(file_path_r_m_c, ',')
+file_2 = read_csv(file_path_rmc, ';')
+
 headers_file_1  = file_1[0].keys()
 headers_file_2  = file_2[0].keys()
 
+def unique_data():
 
-unique_data = []
-for row in file_1:
-    if row not in unique_data:
-        unique_data.append(row)
-
-for row in file_2:
-    if row not in unique_data:
-        unique_data.append(row)
+    unique_data = []
+    for row in file_1:
+        if row not in unique_data:
+            unique_data.append(row)
 
 
-headers_result = []
-for header in headers_file_1:
-    if header not in headers_result:
-        headers_result.append(header)
+    for row in file_2:
+        if row not in unique_data:
+            unique_data.append(row)
 
-for header in headers_file_2:
-    if header not in headers_result:
-        headers_result.append(header)
 
+    headers_result = []
+    for header in headers_file_1:
+        if header not in headers_result:
+            headers_result.append(header)
+
+    for header in headers_file_2:
+        if header not in headers_result:
+            headers_result.append(header)
+
+    return headers_result, unique_data
+
+headers_result, unique_data = unique_data()
 
 result_file_csv = current_dir / 'result_file_csv.csv'
 with open(result_file_csv, 'w', newline='') as csvfile:
